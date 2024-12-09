3aeddf6ce57204e1d84eb1b32ee18054f4de73de
Dakshatk <dakshat25k@gmail.com>
2024-12-10 00:00:18 +0530

    updated

--- majorityelement.cpp
+++ majorityelement.cpp
@@ -1,24 +1,38 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int nums[] = {3, 2, 3};  
-    int n = sizeof(nums) / sizeof(nums[0]);  
-    
-    int candidate = 0;
-    int count = 0;
-    
-    for (int i = 0; i < n; ++i) {
-        if (count == 0) {
-            candidate = nums[i];
+int majorityElement(vector<int> v) {
+
+    //size of the given array:
+    int n = v.size();
+    int cnt = 0; // count
+    int el; // Element
+
+    //applying the algorithm:
+    for (int i = 0; i < n; i++) {
+        if (cnt == 0) {
+            cnt = 1;
+            el = v[i];
         }
-        if (nums[i] == candidate) {
-            count++;
-        } else {
-            count--;
-        }
+        else if (el == v[i]) cnt++;
+        else cnt--;
     }
-    
-    cout << "Majority Element: " << candidate << endl;
+
+    //checking if the stored element
+    // is the majority element:
+    int cnt1 = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == el) cnt1++;
+    }
+
+    if (cnt1 > (n / 2)) return el;
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+    int ans = majorityElement(arr);
+    cout << "The majority element is: " << ans << endl;
     return 0;
 }
